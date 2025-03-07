da14a9a631efa506197158ad5e051aa1823276a5
Yuvraj <yuvrajkumarsingh303@gmail.com>
2025-03-08 00:43:01 +0530

    remove duplicate from list

---
+++ duplicate.py
@@ -0,0 +1,14 @@
+# duplicate deletion
+head = [1,1,2]
+list=[]
+
+for i in head:
+
+    if i not in list:
+        list.append(i)
+
+print(list)
+
+arr = [1,2,2,3,3,3,4,4,4,4]
+new = arr(set(arr))
+print(new)  
